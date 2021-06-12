f74da51ae4ba3290239dc7b135d76bbbe8d85757
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-06-12 09:25:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const Discord = require("discord.js");
-const disbut = require("discord-buttons");
-async function buttonStuff(client, message, pages, timeout){
+
+exports.pages = async function(client, message, pages, timeout, disbut, style){
+    const Discord = require("discord.js");
+    require("discord-buttons");
+    var timeForStart = Date.now();
     if(pages.length >= 5){
         console.error("Amount of pages must be 4 or below!");
         return;
     }
-    var currentPage = 0;
-    let button = new MessageButton()
+    const pageMovingButtons1 = new disbut.MessageButton()
+    .setID(`forward_button`)
+    .setLabel("")
+    .setEmoji("⏩")
+    .setStyle(style)
+    const pageMovingButtons2 = new disbut.MessageButton()
+    .setID(`back_button`)
     .setLabel("")
     .setEmoji("⏪")
-    .setID("back_button")
-    let button2 = new MessageButton()
-    .setLabel("I like")
-    .setEmoji("⏩")
-    .setID("forward_button")
-    const buttonRow = new disbut.MessageActionRow
-    message.edit({components: pages[0]});
-    const filter = (button) => button.clicker.user.id === message.author.id;
-    const collector = m.createButtonCollector(filter, { time: timeout });
-    collector.on("collect", async b =>{
+    .setStyle(style)
+    var pageMovingButtons = new disbut.MessageActionRow()
+    .addComponent(pageMovingButtons2)
+    .addComponent(pageMovingButtons1)
+    var currentPage = 0;
+    var array = pages[currentPage];
+    array.push(pageMovingButtons);
+    message.edit(message.content, {components: array});
+    client.on("clickButton", async b=>{
+        console.log("wee")
+        if(Date.now() - timeForStart >= timeout)return;
+        if(b.message.id == message.id){
         if(b.id == "back_button"){
             if(currentPage - 1 > 0){
                 currentPage = pages.length - 1
@@ -31,10 +40,16 @@ async function buttonStuff(client, message, pages, timeout){
             } else{
                 currentPage += 1;
             }
+            console.log("woo")
         }
         if(b.id == "back_button" || b.id == "forward_button"){
+            var array = pages[currentPage];
+            array.push(pageMovingButtons);
+            message.edit(message.content, {components: array});
             b.defer(true);
-            m.edit({components: pages[currentPage]});
         }
+    } else{
+        b.defer(true);
+    }
     })
 }
